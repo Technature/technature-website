b9ff4f6a40b7b1203ca568219d4113bf7eb1ece3
thomas <tkatsika@gmail.com>
2023-06-14 15:07:24 +0300

    change dummy data

--- helpers/dummyData.js
+++ helpers/dummyData.js
@@ -3,15 +3,15 @@ export const blogPosts = [
     {
         title: "10 signs you probably need a new website",
         tags: ["EXPERTISE", "DESIGN"],
-        photoPath: "/pic1.webp"
+        pathName: "/pic1.webp"
     }
     , {
         title: "How long does it take to design and build a website?",
         tags: ["EXPERTISE",],
-        photoPath: "/pic1.webp"
+        pathName: "/pic1.webp"
     }
     , {
         title: "How long does it take to design and build a website?",
         tags: ["EXPERTISE",],
-        photoPath: "/pic1.webp"
+        pathName: "/pic1.webp"
     }]
