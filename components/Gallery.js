7cd8cf9ad2770d1adf2c22152a8f33f864a5db11
thomas <tkatsika@gmail.com>
2023-06-15 16:58:52 +0300

    responsive

--- components/Gallery.js
+++ components/Gallery.js
@@ -9,24 +9,24 @@ const  Gallery = ({sofia})=>{
         <>
                   {/* SECTION 6 */}
                   <section className=" relative width-full py-40 flex flex-col items-center justify-center bg-zinc-800">
-      <div id="Titles" className="flex flex-row items-center justify-center">
-      <div className=" w-1/3 relative h-[500px]  ">
+      <div id="Titles" className="flex flex-col md:flex-row items-center justify-center">
+      <div className="w-[80%]  md: w-1/3 relative aspect-square  ">
           <Image alt="gallery" src="/work.svg" fill={true}></Image>
         </div>
-        <div className="w-1/3 ml-20 mt-40">
+        <div className="w-1/2 md:w-1/3 md:ml-20 md:mt-40">
           <h1 className={`${sofia.className} text-white tracking-tighter md:text-7xl text-5xl  mb-5 font-extrabold `}>
             Shit-hot work for hot-shot brands
           </h1>
         </div>
       </div>
       
-                <div id="imageGallery" className="w-full flex justify-center mt-20">
+                <div id="imageGallery" className="w-full flex justify-center items-center flex-col md:flex-row mt-20">
                 
-                   <div id="image-card-wrapper" className="w-1/3   m-8">
+                   <div id="image-card-wrapper" className="w-[80%] md:w-1/3   mt-8">
                    <ImageCard src={"/pic1.webp"}></ImageCard>
                 </div>
 
-                <div id="image-card-wrapper" className="w-1/3">
+                <div id="image-card-wrapper" className="w-[80%] md:w-1/3 ">
                    <ImageCard src={"/pic1.webp"}></ImageCard>
                 </div>
                 </div>
